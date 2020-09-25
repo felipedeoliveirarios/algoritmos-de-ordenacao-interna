1bd2ee1e46c8d67dad90d0ec35ef40f9ffb3f4d1
Cássio Coelho <cassioozn@gmail.com>
2020-09-25 18:45:49 -0300

    QuickSort

---
+++ datasets_prontos/quicksort.py
@@ -0,0 +1,20 @@
+from main import notificar
+
+def quicksort(lista, inicio = 0, fim = None):
+    if(fim is None):
+        fim = len(lista) - 1
+    if(inicio < fim):
+        p = partition(lista, inicio, fim)
+        quicksort(lista, inicio, p - 1)
+        quicksort(lista, p + 1, fim)
+
+def partition(lista, inicio, fim):
+    pivo = lista[fim]
+    i = inicio
+    for j in range(inicio, fim):
+        if lista[j] <= pivo:
+            lista[j], lista[i] = lista[i], lista[j]
+            i += 1
+            notificar("mov", 1)
+    lista[i], lista[fim] = lista[fim], lista[inicio]
+    return i
